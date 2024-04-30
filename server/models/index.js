17482508de888043efb94b8c6aa652729c5ab4b2
Lucas Wysoczanski <109931537+LucasWyski001@users.noreply.github.com>
2024-04-30 14:41:07 -0500

    added use

--- server/models/index.js
+++ server/models/index.js
@@ -3,4 +3,8 @@ const Fountain = require('./Fountain');
 
 module.exports = { User, Fountain };
 
+app.get("/", (req,res) =>{
+    res.json("Hello");
+  })
+
 mongoose.connect('mongodb+srv://root:root@cluster0.1hdcug7.mongodb.net/Untitled2');
